52da033b2b6c35557cb4bf052de198a9aeb325d4
= <=>
2024-08-09 14:43:35 +0700

    using fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
@@ -40,7 +40,7 @@ function App() {
 
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -83,7 +83,7 @@ function App() {
           </ul></div>
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
